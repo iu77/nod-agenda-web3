1b08504d0943d419146adc31e5599be4f736ba81
iu77 <ionelungur0225@gmail.com>
2019-01-07 21:40:27 +0200

    node agenda  init

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # Agenda APP
 
 A simple app using node & express for managing phone books
+
+##Setup
+
+```
+
+git clone git@github.com:iu77/nod-agenda-web3.git
+cd node-agenda-web3
+npm install
+```
+
+## Running app
+
+```
+npm run devstart
+```
+npm run devstart
+
+
+
+
+open http://localhost:3000/
---
+++ routes/contacts.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.get('/delete', function(req, res, next) {
+  var phoneToRemove= req.query.phone;
+  res.send("removing contact:" + phoneToRemove);
+});
+
+module.exports = router;
