5957542b5723f4ad98808f7dba0af923b1f80d7a
iu77 <ionelungur0225@gmail.com>
2019-01-26 17:34:12 +0200

    create new contact

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,15 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
+// /contacts/delete?phone=1234
 router.get('/delete', function (req, res, next) {
   var phone = req.query.phone;
 
-  var fs = require('fs');
   var content = fs.readFileSync('public/data/contacts.json');
   var contacts = JSON.parse(content);
 
@@ -19,7 +20,31 @@ router.get('/delete', function (req, res, next) {
 
   content = JSON.stringify(remainingContacts, null, 2);
   fs.writeFileSync('public/data/contacts.json', content);
-  res.json(remainingContacts);
+
+  //res.json({succes:true});
+  //TODP please redirect to agenda.html
+  res.redirect('/agenda.html')
+});
+
+// /contacts/create
+router.post('/create', function (req, res, next) {
+  var firstName = req.body.firstName;
+  var lastName = req.body.lastName;
+  var phone = req.body.phone;
+
+  var content = fs.readFileSync('public/data/contacts.json');
+  var contacts = JSON.parse(content);
+
+  contacts.push({
+    firstName,
+    lastName,
+    phone
+  });
+
+  content = JSON.stringify(contacts, null, 2);
+  fs.writeFileSync('public/data/contacts.json', content);
+
+  res.json({ succes: true });
 });
 
 module.exports = router;
